cbe892894a34220681283b8260918ef0f6490509
Piper Merriam <pipermerriam@gmail.com>
2015-08-06 16:29:11 -0600

    readability and comments

--- murmur.sol
+++ murmur.sol
@@ -10,28 +10,27 @@ contract MurmurHash {
         int constant m = 5;
         int constant n = 0xe6546b64;
 
-        bytes32 _hash;
-        int32 numBits;
+        function digest(bytes key, int seed) returns (bytes32 _hash) {
+                //
+                // Compute and return a Murmur3 Hash.
+                // - https://en.wikipedia.org/wiki/MurmurHash
+                //
+                _hash = bytes32(seed);
 
-
-        function digest(bytes key, int32 len, bytes32 seed) returns (bytes32 _hash) {
-                // Temporary variables
-                uint i;
-                bytes32 k;
-                bytes32 remainingBytes;
-
-                _hash = seed;
-
+                // Compute how many 4-byte chunks are in `key`
                 uint numChunks = key.length / 4;
 
-                // Check if this is an off-by-one error
-                for (i = 0; i < numChunks; i ++) {
-                        // construct the 4-by
+                // Iterate over 4-byte chunks of `key`
+                for (var i = 0; i < numChunks; i ++) {
+                        bytes32 k;
+
+                        // extract the 4-bytes
                         bytes4 b1 = key[(4 * i)] << 3 * 8;
                         bytes4 b2 = key[(4 * i) + 1] << 2 * 8;
                         bytes4 b3 = key[(4 * i) + 2] << 1 * 8;
                         bytes4 b4 = key[(4 * i) + 3];
 
+                        // construct the 4-byte chunk
                         bytes32 fourByteChunk = b1 | b2 | b3 | b4;
 
                         k = fourByteChunk;
@@ -43,25 +42,30 @@ contract MurmurHash {
                         _hash = bytes32(int(_hash) * m + n);
                 }
 
+                // If there is a `tail` leftover that was not processed as a
+                // 4-byte chunk, deal with it.
                 if ( key.length % 4 > 0 ) {
-                        uint tailLength = key.length % 4;
+                        bytes32 tail;
 
-                        for (i = 0; i < tailLength; i++) {
+                        for (i = 0; i < key.length % 4; i++) {
                                 // This explicitely swaps the order of the bytes (which
                                 // should only be done on big-endian machines.
-                                // TODO: is this big endian?
-                                remainingBytes = remainingBytes | key[key.length - i];
-                                remainingBytes = remainingBytes << 8;
+                                //
+                                // TODO: is this big endian.  If not, then this
+                                // needs to be switched to `key[numChunks * 4 +
+                                // i]` to grab the bytes in the reverse order.
+                                tail = tail | key[key.length - i];
+                                tail = tail << 8;
                         }
 
-                        remainingBytes = bytes32(int(remainingBytes) * c1);
-                        remainingBytes = remainingBytes << r1;
-                        remainingBytes = bytes32(int(remainingBytes) * c2);
+                        tail = bytes32(int(tail) * c1);
+                        tail = tail << r1;
+                        tail = bytes32(int(tail) * c2);
 
-                        _hash = _hash ^ remainingBytes;
+                        _hash = _hash ^ tail;
                 }
 
-                _hash = _hash ^ bytes32(len);
+                _hash = _hash ^ bytes32(key.length);
                 _hash = _hash ^ (_hash >> 16);
                 _hash = bytes32(int(_hash) * 0x85ebca6b);
                 _hash = _hash ^ (_hash >> 13);
